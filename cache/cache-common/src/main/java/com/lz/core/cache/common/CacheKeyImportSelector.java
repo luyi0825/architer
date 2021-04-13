e1e288610102ec4f8d42d2d48da16b22e65c27c3
luyi <893431435@qq.com>
2021-04-14 00:55:41 +0800

    缓存key导入选择器

---
+++ cache/cache-common/src/main/java/com/lz/core/cache/common/CacheKeyImportSelector.java
@@ -0,0 +1,62 @@
+package com.lz.core.cache.common;
+
+import com.lz.core.cache.common.aspectj.AspectjConfiguration;
+import com.lz.core.cache.common.enums.KeyStrategy;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.GenericTypeResolver;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.Assert;
+
+import java.util.function.Predicate;
+
+/**
+ * @author luyi
+ * 缓存key导入选择器
+ * @see org.springframework.context.annotation.AdviceModeImportSelector   模仿的这个类
+ */
+public class CacheKeyImportSelector implements ImportSelector {
+
+    /**
+     * key生成策越
+     */
+    private static final String KEY_STRATEGY = "keyStrategy";
+
+
+    /**
+     * 选择导入
+     *
+     * @param importingClassMetadata 导出的注解
+     * @return 需要注入的类
+     */
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        Class<?> annType = GenericTypeResolver.resolveTypeArgument(getClass(), CacheKeyImportSelector.class);
+        Assert.state(annType != null, "Unresolvable type argument for AdviceModeImportSelector");
+        AnnotationAttributes attributes = AnnotationAttributes.fromMap(importingClassMetadata.getAnnotationAttributes(annType.getName(), false));
+        if (attributes == null) {
+            throw new IllegalArgumentException(String.format(
+                    "@%s is not present on importing class '%s' as expected",
+                    annType.getSimpleName(), importingClassMetadata.getClassName()));
+        }
+        KeyStrategy keyStrategy = attributes.getEnum(KEY_STRATEGY);
+        String[] imports = selectKeyStrategyImports(keyStrategy);
+        if (imports == null) {
+            throw new IllegalArgumentException("Unknown keyStrategy: " + keyStrategy);
+        }
+        return imports;
+    }
+
+    private String[] selectKeyStrategyImports(KeyStrategy keyStrategy) {
+        return null;
+    }
+
+
+    @Override
+    public Predicate<String> getExclusionFilter() {
+        return null;
+    }
+
+
+}
