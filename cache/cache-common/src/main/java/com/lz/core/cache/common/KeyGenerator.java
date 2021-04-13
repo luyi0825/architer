c62f706bffab46161ad9b349ac873abc8a1b0f94
luyi <893431435@qq.com>
2021-04-14 00:44:24 +0800

    完善注释

--- cache/cache-common/src/main/java/com/lz/core/cache/common/KeyGenerator.java
+++ cache/cache-common/src/main/java/com/lz/core/cache/common/KeyGenerator.java
@@ -7,7 +7,13 @@ import java.lang.reflect.Method;
  * 缓存key 生成器
  */
 public interface KeyGenerator {
-
+    /**
+     * 得到key
+     *
+     * @param method 方法
+     * @param args   参数
+     * @return 缓存的key
+     */
     String getKey(Method method, Object[] args);
 
 }
