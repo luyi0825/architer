e3bc16e9ddd95d998e503da7c2c91e7caaa42d00
luyi <893431435@qq.com>
2021-04-14 00:56:24 +0800

    注解key策略

---
+++ cache/cache-common/src/main/java/com/lz/core/cache/common/AnnotationsKeyGenerator.java
@@ -0,0 +1,15 @@
+package com.lz.core.cache.common;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author luyi
+ * @see com.lz.core.cache.common.enums.KeyStrategy#ANNOTATIONS
+ * 注解策略
+ */
+public class AnnotationsKeyGenerator implements KeyGenerator {
+    @Override
+    public String getKey(Method method, Object[] args) {
+        return null;
+    }
+}
