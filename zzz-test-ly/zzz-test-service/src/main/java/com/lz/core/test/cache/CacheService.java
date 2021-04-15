96da3c8f3d4a9218c9400771f009e500118a481d
luyi <893431435@qq.com>
2021-04-15 15:25:53 +0800

    add authority

--- zzz-test-ly/zzz-test-service/src/main/java/com/lz/core/test/cache/CacheService.java
+++ zzz-test-ly/zzz-test-service/src/main/java/com/lz/core/test/cache/CacheService.java
@@ -1,11 +1,11 @@
 package com.lz.core.test.cache;
 
-import com.lz.core.cache.common.annotation.Caching;
+import com.lz.core.cache.common.annotation.PutCache;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CacheService {
-    @Caching
+    @PutCache
     public String getXxx(String s) {
         System.out.println("查询数据库");
         return s;
