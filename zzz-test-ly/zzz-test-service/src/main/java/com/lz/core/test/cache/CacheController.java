df8113bd30fbcfb50e97ab37297a9735bf4d4230
luyi <893431435@qq.com>
2021-04-16 01:34:17 +0800

    测试修改

--- zzz-test-ly/zzz-test-service/src/main/java/com/lz/core/test/cache/CacheController.java
+++ zzz-test-ly/zzz-test-service/src/main/java/com/lz/core/test/cache/CacheController.java
@@ -3,13 +3,15 @@ package com.lz.core.test.cache;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *描述：缓存controller
- *@author luyi
- *@date 2021/4/14
+ * 描述：缓存controller
+ *
+ * @author luyi
+ * @date 2021/4/14
  */
 @RestController
 @RequestMapping("/cache")
@@ -17,8 +19,19 @@ public class CacheController {
     @Autowired
     private CacheService cacheService;
 
-    @RequestMapping("/test")
-    public String test(){
-      return  cacheService.getXxx("666");
+    @RequestMapping("/get/{id}")
+    public String get(@PathVariable String id) {
+        return cacheService.getXxx(id);
+    }
+
+    @RequestMapping("/put/{id}")
+    public void put(@PathVariable String id) {
+        cacheService.put(id);
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable String id) {
+        cacheService.delete(id);
+        return "true";
     }
 }
