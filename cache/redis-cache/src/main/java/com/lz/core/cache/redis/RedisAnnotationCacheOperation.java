774840280f48d4a03bee61f0d649962d4c96e2f6
luyi <893431435@qq.com>
2021-04-16 00:31:31 +0800

    redis注解缓存修改

---
+++ cache/redis-cache/src/main/java/com/lz/core/cache/redis/RedisAnnotationCacheOperation.java
@@ -0,0 +1,37 @@
+package com.lz.core.cache.redis;
+
+import com.lz.core.cache.common.AnnotationCacheOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * redis注解缓存操作
+ * @author luyi
+ */
+public class RedisAnnotationCacheOperation implements AnnotationCacheOperation {
+
+    private StringRedisService stringRedisService;
+
+
+    @Override
+    public Object getCache(String key) {
+        return stringRedisService.get(key);
+    }
+
+    @Override
+    public Object putCache(String key, Object value) {
+        stringRedisService.set(key, value);
+        return value;
+    }
+
+    @Override
+    public void deleteCache(String key) {
+        stringRedisService.delete(key);
+    }
+
+    @Autowired
+    public void setStringRedisService(StringRedisService stringRedisService) {
+        this.stringRedisService = stringRedisService;
+    }
+
+
+}
--- cache/redis-cache/src/main/java/com/lz/core/cache/redis/RedisConfig.java
+++ cache/redis-cache/src/main/java/com/lz/core/cache/redis/RedisConfig.java
@@ -8,14 +8,8 @@
 
 package com.lz.core.cache.redis;
 
-import com.lz.core.cache.common.CacheProcess;
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -23,36 +17,19 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 /**
  * Redis配置
  *
- * @author Mark sunlightcs@gmail.com
+ * @author luyi
  */
 @Configuration
 public class RedisConfig {
-
-    /**
-     * 描述:配置单节点redission
-     *
-     * @author luyi
-     * @date 2020/12/25 下午11:45
-     */
     @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
-        RedissonClient redisson = Redisson.create(config);
-        return redisson;
-    }
-
-    @Bean
-   // @ConditionalOnBean(StringRedisTemplate.class)
+    @Autowired(required = false)
     public StringRedisService stringRedisService(StringRedisTemplate redisTemplate) {
         return new StringRedisService(redisTemplate);
     }
 
     @Bean
-//    @ConditionalOnMissingBean
-//    @ConditionalOnBean(StringRedisService.class)
-    public CacheProcess redisCacheCacheProcess() {
-        return new RedisCacheCacheProcessImpl();
+    public RedisAnnotationCacheOperation redisAnnotationCacheOperation() {
+        return new RedisAnnotationCacheOperation();
     }
 
 }
