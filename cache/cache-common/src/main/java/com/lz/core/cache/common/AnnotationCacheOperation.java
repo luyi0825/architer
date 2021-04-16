b38823f2bdcdd7d4a9e451281311ee2be6943b51
luyi <893431435@qq.com>
2021-04-16 22:15:16 +0800

    完善注释，格式代码

--- cache/cache-common/src/main/java/com/lz/core/cache/common/AnnotationCacheOperation.java
+++ cache/cache-common/src/main/java/com/lz/core/cache/common/AnnotationCacheOperation.java
@@ -6,10 +6,28 @@ package com.lz.core.cache.common;
  */
 public interface AnnotationCacheOperation {
 
+    /**
+     * 得到缓存
+     *
+     * @param key 缓存的key
+     * @return 缓存的数据
+     */
     Object getCache(String key);
 
+    /**
+     * 放入缓存
+     *
+     * @param key   缓存的key
+     * @param value 需要缓存的值
+     * @return 同value
+     */
     Object putCache(String key, Object value);
 
+    /**
+     * 删除缓存的值
+     *
+     * @param key 缓存的key
+     */
     void deleteCache(String key);
 
 }
