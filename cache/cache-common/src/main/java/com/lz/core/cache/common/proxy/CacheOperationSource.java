6e32dbc466783a0b39bc2b2fe3eaa0d0c7a80183
luyi <893431435@qq.com>
2021-04-18 12:56:41 +0800

    缓存操作source

---
+++ cache/cache-common/src/main/java/com/lz/core/cache/common/proxy/CacheOperationSource.java
@@ -0,0 +1,33 @@
+package com.lz.core.cache.common.proxy;
+
+import com.lz.core.cache.common.CacheOperation;
+import org.springframework.lang.Nullable;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+/**
+ * @author luyi
+ * @see org.springframework.cache.interceptor.CacheOperationSource 模仿的这个类
+ */
+public interface CacheOperationSource {
+    /**
+     * 类是否满足
+     *
+     * @param targetClass 目标类
+     * @return 如果已知该类在类或方法级别没有缓存操作元数据，则返回false否则返回false 。 否则为tru
+     */
+    default boolean isCandidateClass(Class<?> targetClass) {
+        return true;
+    }
+
+    /**
+     * 得到缓存操作
+     *
+     * @param method      方法
+     * @param targetClass 目标类
+     * @return 解析的注解操作
+     */
+    @Nullable
+    Collection<CacheOperation> getCacheOperations(Method method, @Nullable Class<?> targetClass);
+}
