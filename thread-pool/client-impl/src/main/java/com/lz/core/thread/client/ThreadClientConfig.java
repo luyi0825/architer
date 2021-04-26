5670a99c2a836f7a17277fe73c59ccccd5ad3583
luyi <893431435@qq.com>
2021-04-26 14:16:55 +0800

    客户端属性配置

--- thread-pool/client-impl/src/main/java/com/lz/core/thread/client/ThreadClientConfig.java
+++ thread-pool/client-impl/src/main/java/com/lz/core/thread/client/ThreadClientConfig.java
@@ -2,6 +2,8 @@ package com.lz.core.thread.client;
 
 
 import com.lz.core.thread.client.parse.ThreadPoolParser;
+import com.lz.core.thread.client.properties.ClientProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @author luyi
  */
 @Configuration
+@EnableConfigurationProperties(ClientProperties.class)
 public class ThreadClientConfig {
 
     @Bean
--- thread-pool/client-impl/src/main/java/com/lz/core/thread/client/parse/ThreadPoolParser.java
+++ thread-pool/client-impl/src/main/java/com/lz/core/thread/client/parse/ThreadPoolParser.java
@@ -2,24 +2,33 @@ package com.lz.core.thread.client.parse;
 
 
 import com.lz.core.thread.client.annotation.ThreadPoolClient;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author luyi
  */
+@Log4j2
 public class ThreadPoolParser implements ApplicationContextAware {
 
-    private ApplicationContext applicationContext;
+    List<ThreadPoolTaskExecutor> threadPoolTaskExecutors;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         //得到所有需要监控的线程池
         Map<String, Object> map = applicationContext.getBeansWithAnnotation(ThreadPoolClient.class);
-        map.values();
+        map.forEach((key, value) -> {
+            if (!(value instanceof ThreadPoolTaskExecutor)) {
+                throw new RuntimeException(value.getClass().getName() + " is invalid for ThreadPoolClient");
+            }
+            threadPoolTaskExecutors.add((ThreadPoolTaskExecutor) value);
+        });
     }
 
 
