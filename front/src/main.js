6e29dcd533097ce6f94354d1a723738a9de6f356
luyi <893431435@qq.com>
2021-04-29 01:26:43 +0800

    feat:front init

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,21 @@
-import {createApp} from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+// vuex的实例化对象
+import store from './store'
+import router from './router/router'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
+import VueCompositionApi from '@vue/composition-api'
+import axios from '@/router/axios';
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
+//放到原型对象中
+Vue.prototype.$http = axios;
+Vue.use(ElementPlus)
+Vue.use(element)
+//使用vue3.0
+Vue.use(VueCompositionApi);
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
---
+++ front/src/page/login/codelogin.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+rere
+  </div>
+
+</template>
