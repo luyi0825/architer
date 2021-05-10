03ff3d49898c0b656663eec2d4bb1c29896b4843
luyi <893431435@qq.com>
2021-05-10 18:16:32 +0800

    feat:add Druid包

---
+++ core/db/druid/src/main/java/com/lz/core/druid/DruidConfig.java
@@ -0,0 +1,38 @@
+package com.lz.core.druid;
+
+
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author luyi
+ * 阿里Druid配置
+ */
+@Configuration
+public class DruidConfig {
+
+    public DruidConfig(){
+        System.out.println("init DruidConfig");
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDefaultAutoCommit(false);
+        //配置公用监控数据
+        druidDataSource.setUseGlobalDataSourceStat(true);
+        return druidDataSource;
+    }
+
+    @Bean
+    public StatFilter statFilter() {
+        StatFilter statFilter = new StatFilter();
+        statFilter.setSlowSqlMillis(500);
+        statFilter.setLogSlowSql(true);
+        return statFilter;
+    }
+}
