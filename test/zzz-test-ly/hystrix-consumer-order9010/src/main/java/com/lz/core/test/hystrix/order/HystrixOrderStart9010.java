e41fd958dea33c0e59c304600142b47b88f5d914
luyi <893431435@qq.com>
2021-05-17 23:27:27 +0800

    test:远程调用pay

--- test/zzz-test-ly/hystrix-consumer-order9010/src/main/java/com/lz/core/test/hystrix/order/HystrixOrderStart9010.java
+++ test/zzz-test-ly/hystrix-consumer-order9010/src/main/java/com/lz/core/test/hystrix/order/HystrixOrderStart9010.java
@@ -3,6 +3,7 @@ package com.lz.core.test.hystrix.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author luyi
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class HystrixOrderStart9010 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixOrderStart9010.class, args);
