14dedc3f8e603ebafe60a2db8e1e1d7be8e6f234
luyi <893431435@qq.com>
2021-05-20 09:20:48 +0800

    feat:boot-rest-template

---
+++ core/boot/boot-rest-template/src/main/java/com/lz/core/resttemplate/RestTemplateConfigure.java
@@ -0,0 +1,21 @@
+package com.lz.core.resttemplate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * restTemplate 配置文件
+ *
+ * @author luyi
+ * @date 2021/5/19
+ */
+@Configuration
+public class RestTemplateConfigure {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
