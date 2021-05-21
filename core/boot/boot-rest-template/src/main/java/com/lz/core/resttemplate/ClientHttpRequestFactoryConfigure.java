00dd8ce258e19cab703dc7b9930530aa57eb7cd3
luyi <893431435@qq.com>
2021-05-21 17:53:46 +0800

    feat:增加ClientHttpRequestFactoryConfigure

---
+++ core/boot/boot-rest-template/src/main/java/com/lz/core/resttemplate/ClientHttpRequestFactoryConfigure.java
@@ -0,0 +1,9 @@
+package com.lz.core.resttemplate;
+
+/**
+ * RestTemplate客户端请求工厂配置
+ *
+ * @author luyi
+ */
+public class ClientHttpRequestFactoryConfigure {
+}
--- core/boot/boot-rest-template/src/main/java/com/lz/core/resttemplate/RestTemplateConfigure.java
+++ core/boot/boot-rest-template/src/main/java/com/lz/core/resttemplate/RestTemplateConfigure.java
@@ -3,6 +3,7 @@ package com.lz.core.resttemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -16,6 +17,10 @@ public class RestTemplateConfigure {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate= new RestTemplate();
+        restTemplate.setRequestFactory();
     }
+
+
+
 }
