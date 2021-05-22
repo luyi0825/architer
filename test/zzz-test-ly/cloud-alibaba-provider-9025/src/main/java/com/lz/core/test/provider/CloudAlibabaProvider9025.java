162593a85fbca5a7923bd258d5f8f01581f20642
luyi <893431435@qq.com>
2021-05-22 22:10:30 +0800

    feat: cloud alibaba 修改

--- test/zzz-test-ly/cloud-alibaba-provider-9025/src/main/java/com/lz/core/test/provider/CloudAlibabaProvider9025.java
+++ test/zzz-test-ly/cloud-alibaba-provider-9025/src/main/java/com/lz/core/test/provider/CloudAlibabaProvider9025.java
@@ -1,8 +1,11 @@
 package com.lz.core.test.provider;
 
+import com.lz.core.test.provider.config.UserConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * springCloudAlibaba 提供者启动类
@@ -11,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(UserConfig.class)
 public class CloudAlibabaProvider9025 {
     public static void main(String[] args) {
         SpringApplication.run(CloudAlibabaProvider9025.class, args);
