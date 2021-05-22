ed9279a8a55a3ed940be608d3342c5751aa125bc
luyi <893431435@qq.com>
2021-05-22 13:43:02 +0800

    feat:add cloud-stream

---
+++ test/zzz-test-ly/stream-rabbitmq-consumer9021/src/main/java/com/lz/core/test/stream/comsumer/StreamRabbitConsumer9021.java
@@ -0,0 +1,17 @@
+package com.lz.core.test.stream.comsumer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author luyi
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StreamRabbitConsumer9021 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitConsumer9021.class, args);
+    }
+}
