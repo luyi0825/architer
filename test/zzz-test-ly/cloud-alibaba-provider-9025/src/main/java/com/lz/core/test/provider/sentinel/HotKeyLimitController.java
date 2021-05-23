429379ab7a34390a97605248b0bd8f582b90d903
luyi <893431435@qq.com>
2021-05-23 22:34:22 +0800

    feat: 增加热点key限流测试controller

---
+++ test/zzz-test-ly/cloud-alibaba-provider-9025/src/main/java/com/lz/core/test/provider/sentinel/HotKeyLimitController.java
@@ -0,0 +1,11 @@
+package com.lz.core.test.provider.sentinel;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 热点key限流对应的测试controller
+ * @author luyi
+ */
+@RestController
+public class HotKeyLimitController {
+}
