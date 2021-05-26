ca3102066dc5895d2eade75c6bbe71f93d562182
luyi <893431435@qq.com>
2021-05-26 15:26:42 +0800

    fix:完善参数校验异常

--- core/service/service-common/src/main/java/com/lz/core/service/exception/ParamsValidException.java
+++ core/service/service-common/src/main/java/com/lz/core/service/exception/ParamsValidException.java
@@ -5,5 +5,25 @@ package com.lz.core.service.exception;
  *
  * @author luyi
  */
-public class ParamsValidException {
+public class ParamsValidException extends RuntimeException {
+
+    public ParamsValidException() {
+        super();
+    }
+
+    public ParamsValidException(String message) {
+        super(message);
+    }
+
+    public ParamsValidException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParamsValidException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ParamsValidException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
