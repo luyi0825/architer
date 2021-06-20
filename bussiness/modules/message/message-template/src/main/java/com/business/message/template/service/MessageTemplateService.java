2c58fc9bfbf782efee522bc86c7b540211c8d84e
luyi <893431435@qq.com>
2021-06-21 01:10:11 +0800

    feat:增加消息服务模板service

---
+++ bussiness/modules/message/message-template/src/main/java/com/business/message/template/service/MessageTemplateService.java
@@ -0,0 +1,42 @@
+package com.business.message.template.service;
+
+
+import com.business.message.template.entity.MessageTemplate;
+import com.core.mybatisplus.service.BaseService;
+
+
+/**
+ * @author luyi
+ * @date 2021/1/22
+ * 消息模板
+ */
+public interface MessageTemplateService extends BaseService<MessageTemplate> {
+    /**
+     * 通过code找到对应的消息模板
+     *
+     * @param templateCode 模板code
+     * @return 消息模板配置信息
+     */
+    MessageTemplate getMessageTemplateByCode(String templateCode);
+
+    /**
+     * 删除消息模板
+     *
+     * @param templateCode 模板编码
+     */
+    void deleteMessageTemplate(String templateCode);
+
+    /**
+     * 添加消息模板
+     *
+     * @param messageTemplate 模板信息
+     */
+    void addMessageTemplate(MessageTemplate messageTemplate);
+
+    /**
+     * 修改消息模板
+     *
+     * @param messageTemplate 模板信息
+     */
+    void updateMessageTemplate(MessageTemplate messageTemplate);
+}
