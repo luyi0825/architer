5dd4f078f10fa5b3060f7d72178c584478db4e0f
luyi <893431435@qq.com>
2021-06-20 16:05:02 +0800

    feat:初始化message服务

---
+++ bussiness/server/message-server/src/main/java/com/business/message/MessageServer.java
@@ -0,0 +1,20 @@
+package com.business.message;
+
+import com.core.boot.MoulesBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消息服务
+ *
+ * @author luyi
+ */
+@SpringBootApplication
+public class MessageServer {
+
+    public static void main(String[] args) {
+        Class<?>[] modules = new MoulesBuilder().buildMoules(MessageServer.class);
+        new SpringApplication(modules).run(args);
+    }
+
+}
