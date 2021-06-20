3395e6f7c2aca335b1146ac3cdf6a9ac09b647be
luyi <893431435@qq.com>
2021-06-21 01:10:56 +0800

    feat:增加消息服务模板service-impl

---
+++ bussiness/modules/message/message-template-impl/src/main/java/com/business/message/template/MessageTemplateModule.java
@@ -0,0 +1,12 @@
+package com.business.message.template;
+
+import com.core.module.common.Module;
+import org.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author luyi
+ */
+@MapperScan("com.business.message.template.dao")
+@Module(name="message-template",caption = "消息服务-模板模块")
+public class MessageTemplateModule {
+}
---
+++ bussiness/modules/message/message-template-impl/src/main/java/com/business/message/template/service/impl/EmailMessageSendAdaptor.java
@@ -0,0 +1,24 @@
+package com.business.message.mail.service.impl;
+
+
+
+import org.springframework.stereotype.Component;
+
+/**
+ * email消息发送适配器
+ *
+ * @author ly
+ */
+@Component
+public class EmailMessageSendAdaptor {
+
+    public boolean support(String messageType) {
+        return true;
+       // return MessageConstants.MESSAGE_TYPE_EMAIL.equals(messageType);
+    }
+
+    public void sendMessage(String message, String templateCode) {
+
+    }
+
+}
