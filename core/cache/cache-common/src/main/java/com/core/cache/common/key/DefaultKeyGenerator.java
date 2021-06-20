febb2dd98fe6c598ae00b73c0324ad32aa0181b2
luyi <893431435@qq.com>
2021-06-20 11:02:28 +0800

    fix:缓存key生成修改

--- core/cache/cache-common/src/main/java/com/core/cache/common/key/DefaultKeyGenerator.java
+++ core/cache/cache-common/src/main/java/com/core/cache/common/key/DefaultKeyGenerator.java
@@ -5,7 +5,6 @@ import com.core.cache.common.exception.CacheAnnotationIllegalException;
 import com.core.cache.common.operation.CacheOperation;
 import com.core.cache.common.operation.CacheOperationMetadata;
 import com.core.cache.common.CacheExpressionParser;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Objects;
@@ -13,7 +12,6 @@ import java.util.Objects;
 /**
  * @author luyi
  * 默认缓存key 生成器
- * @TODO 多个cacheName的处理
  */
 public class DefaultKeyGenerator implements KeyGenerator {
 
@@ -31,16 +29,17 @@ public class DefaultKeyGenerator implements KeyGenerator {
     @Override
     public String getKey(CacheOperationMetadata metadata) {
         CacheOperation cacheOperation = metadata.getCacheOperation();
-        String[] cacheName = cacheOperation.getCacheName();
+        String cacheName = cacheOperation.getCacheName();
         String cacheKey = cacheOperation.getKey();
         if (StringUtils.isEmpty(cacheKey)) {
             throw new CacheAnnotationIllegalException("cacheKey is null");
         }
         String cacheSuffix = Objects.requireNonNull(cacheExpressionParser.executeParse(metadata, cacheKey)).toString();
-        if (ArrayUtils.isEmpty(cacheName)) {
+        if (StringUtils.isEmpty(cacheName)) {
             return cacheSuffix;
         } else {
-            return cacheName[0] + getSeparator() + cacheSuffix;
+            String cachePrefix = Objects.requireNonNull(cacheExpressionParser.executeParse(metadata, cacheName)).toString();
+            return cachePrefix + getSeparator() + cacheSuffix;
         }
     }
 
