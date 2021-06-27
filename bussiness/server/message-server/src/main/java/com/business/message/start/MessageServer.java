d0ff308cd6bb25934c6396d3deadfff79c969c06
luyi <893431435@qq.com>
2021-06-27 17:03:37 +0800

    fix:增加MapperScan

--- bussiness/server/message-server/src/main/java/com/business/message/start/MessageServer.java
+++ bussiness/server/message-server/src/main/java/com/business/message/start/MessageServer.java
@@ -1,7 +1,8 @@
 package com.business.message.start;
 
-import com.core.boot.MoulesBuilder;
+import com.core.starter.web.module.MoulesBuilder;
 import com.core.cache.common.annotation.CustomEnableCaching;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @CustomEnableCaching
 @EnableDiscoveryClient
+@MapperScan("com.business.message.*.dao")
 public class MessageServer {
     public static void main(String[] args) {
         Class<?>[] modules = new MoulesBuilder().buildMoules(MessageServer.class);
