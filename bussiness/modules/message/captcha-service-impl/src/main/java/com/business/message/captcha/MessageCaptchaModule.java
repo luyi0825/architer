47807505630001f6d9965159a3204c158c088426
luyi <893431435@qq.com>
2021-06-27 16:58:20 +0800

    feat:增加消息服务，验证码模块

---
+++ bussiness/modules/message/captcha-service-impl/src/main/java/com/business/message/captcha/MessageCaptchaModule.java
@@ -0,0 +1,13 @@
+package com.business.message.captcha;
+
+
+import com.core.module.common.Module;
+
+/**
+ * 增加消息服务，验证码模块
+ *
+ * @author luyi
+ */
+@Module(name = "message-captcha", caption = "消息服务-验证码模块")
+public class MessageCaptchaModule {
+}
