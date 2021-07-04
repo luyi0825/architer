51f9654dd894afcdc3fa18f5cfd2d70265b2e2cc
luyi <893431435@qq.com>
2021-07-05 02:02:09 +0800

    feat:search配置初始化

---
+++ bussiness/server/search-server/src/main/java/com/business/search/start/RestHighLevelClientConfig.java
@@ -0,0 +1,31 @@
+package com.business.search.start;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author luyi
+ */
+@Configuration
+public class RestHighLevelClientConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+    @Bean
+    public RestHighLevelClient esClient() {
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("124.71.110.204", 9200)));
+    }
+}
