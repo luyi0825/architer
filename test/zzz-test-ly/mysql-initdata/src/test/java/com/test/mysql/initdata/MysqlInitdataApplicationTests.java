44a67dc97f4d7088b0c7847a79a309f651d22d4e
luyi <893431435@qq.com>
2021-07-13 20:38:45 +0800

    feat:sql-init初始化

---
+++ test/zzz-test-ly/mysql-initdata/src/test/java/com/test/mysql/initdata/MysqlInitdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mysql.initdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlInitdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
