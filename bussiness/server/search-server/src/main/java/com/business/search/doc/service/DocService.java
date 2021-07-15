2a39921afb779f05138efc641b04d120e5348610
luyi <893431435@qq.com>
2021-07-15 16:15:10 +0800

    fix:service修改

--- bussiness/server/search-server/src/main/java/com/business/search/doc/service/DocService.java
+++ bussiness/server/search-server/src/main/java/com/business/search/doc/service/DocService.java
@@ -1,7 +1,10 @@
 package com.business.search.doc.service;
 
+import com.business.search.doc.model.DocumentRequest;
+import com.business.search.doc.model.DocumentResponse;
+
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 
 /**
  * 数据查询的service
@@ -9,12 +12,17 @@ import java.util.Map;
  * @author luyi
  */
 public interface DocService {
-    public Object queryList(String index) throws IOException;
+
+    DocumentRequest insert(String index, DocumentRequest docs) throws IOException;
+
+    DocumentResponse findById(String index, String id) throws IOException;
+
+    List<DocumentResponse> queryList(String index) throws IOException;
 
     /**
      * @param index
-     * @param docs
+     * @param documentRequest
      * @throws IOException
      */
-    void put(String index, Map<String, Object> docs) throws IOException;
+    void update(String index, DocumentRequest documentRequest) throws IOException;
 }
--- bussiness/server/search-server/src/main/java/com/business/search/doc/service/impl/DocServiceImpl.java
+++ bussiness/server/search-server/src/main/java/com/business/search/doc/service/impl/DocServiceImpl.java
@@ -1,17 +1,28 @@
 package com.business.search.doc.service.impl;
 
+import com.business.search.doc.model.DocumentRequest;
+import com.business.search.doc.model.DocumentResponse;
 import com.business.search.doc.service.DocService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author luyi
@@ -22,18 +33,55 @@ public class DocServiceImpl implements DocService {
 
     private RestHighLevelClient restHighLevelClient;
 
+    private static final String ID_KEY = "id";
+
     @Override
-    public Object queryList(String index) throws IOException {
-        SearchRequest searchRequest = new SearchRequest(index);
-        return restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT).getHits();
+    public DocumentRequest insert(String index, DocumentRequest docs) throws IOException {
+        IndexRequest indexRequest = new IndexRequest();
+        indexRequest.index(index);
+        if (StringUtils.hasText(docs.getId())) {
+            indexRequest.id(docs.getId());
+        }
+        indexRequest.source(docs.getSource());
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+        System.out.println(indexResponse.getResult().getOp());
+        docs.setId(indexResponse.getId());
+        return docs;
     }
 
     @Override
-    public void put(String index, Map<String, Object> docs) throws IOException {
-        UpdateRequest request = new UpdateRequest();
+    public DocumentResponse findById(String index, String id) throws IOException {
+        GetRequest getRequest = new GetRequest(index, id);
+        GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        DocumentResponse documentResponse = new DocumentResponse(getResponse.getSource());
+        documentResponse.put(ID_KEY, getResponse.getId());
+        return documentResponse;
+    }
+
+    @Override
+    public List<DocumentResponse> queryList(String index) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
+        if (searchHits != null) {
+            List<DocumentResponse> documents = new ArrayList<>(searchHits.length);
+            for (SearchHit hit : searchHits) {
+                DocumentResponse documentResponse = new DocumentResponse(hit.getSourceAsMap());
+                documentResponse.put(ID_KEY, hit.getId());
+                documents.add(documentResponse);
+            }
+            return documents;
+        }
+        return null;
+    }
+
+    @Override
+    public void update(String index, DocumentRequest documentRequest) throws IOException {
+        UpdateRequest request = new UpdateRequest(index, documentRequest.getId());
         ObjectMapper objectMapper = new ObjectMapper();
-        request.doc(objectMapper.writeValueAsString(docs), XContentType.JSON);
-        restHighLevelClient.update(request, RequestOptions.DEFAULT);
+        request.doc(objectMapper.writeValueAsString(documentRequest), XContentType.JSON);
+        UpdateResponse updateResponse = restHighLevelClient.update(request, RequestOptions.DEFAULT);
+        System.out.println(updateResponse.getResult().getOp());
     }
 
     @Autowired
