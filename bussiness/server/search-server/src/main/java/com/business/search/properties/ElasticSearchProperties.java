3e8adf828f379d52f55f5a07bdefc6e82241d2e9
luyi <893431435@qq.com>
2021-07-15 16:13:13 +0800

    feat:增加用户名，密码配置

--- bussiness/server/search-server/src/main/java/com/business/search/properties/ElasticSearchProperties.java
+++ bussiness/server/search-server/src/main/java/com/business/search/properties/ElasticSearchProperties.java
@@ -10,6 +10,17 @@ import java.util.List;
  */
 @ConfigurationProperties("customize.es")
 public class ElasticSearchProperties {
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 集群地址
+     */
     private List<EsHost> hosts;
     /**
      * 默认的数据分片数
@@ -19,6 +30,23 @@ public class ElasticSearchProperties {
      * 默认的数据备份数
      */
     private int defaultReplicas = 1;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public List<EsHost> getHosts() {
         return hosts;
@@ -43,7 +71,6 @@ public class ElasticSearchProperties {
     public void setDefaultReplicas(int defaultReplicas) {
         this.defaultReplicas = defaultReplicas;
     }
-
 
 
 }
