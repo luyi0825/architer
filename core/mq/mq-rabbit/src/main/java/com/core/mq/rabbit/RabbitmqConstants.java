511b9773b7fd0ca7130925f59ece1bd2827752f7
luyi <893431435@qq.com>
2021-07-17 17:34:03 +0800

    feat:增加mq的常量

---
+++ core/mq/mq-rabbit/src/main/java/com/core/mq/rabbit/RabbitmqConstants.java
@@ -0,0 +1,12 @@
+package com.core.mq.rabbit;
+
+public final class RabbitmqConstants {
+    /**
+     * 重试次数
+     */
+    public static final String TRY_COUNT_KEY = "try_count";
+    /**
+     * 最大重试次数
+     */
+    public static final String MAX_TRY_COUNT_KEY = "max_try_count";
+}
