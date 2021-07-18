78e3ae23bcba32890384393e74277c9db95a39d1
luyi <893431435@qq.com>
2021-07-18 13:55:48 +0800

    fix:同步结果dao修改

--- core/db/elastic-search/elastic-search-sync-consume/src/main/java/com/core/es/consumer/dao/SyncResultDao.java
+++ core/db/elastic-search/elastic-search-sync-consume/src/main/java/com/core/es/consumer/dao/SyncResultDao.java
@@ -4,11 +4,16 @@ import com.core.es.consumer.entiry.SyncResult;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
+/**
+ * @author luyi
+ * 同步结果dao层
+ */
 @Repository
 public interface SyncResultDao extends MongoRepository<SyncResult, Long> {
     SyncResult findByBusinessKeyAndBatchId(String businessKey, String batchId);
-    List<SyncResult> findByBatchIdAndBatchIdIn(String businessKey, String[] batchIds);
 
+    List<SyncResult> findByBusinessKeyAndBatchIdIn(String businessKey, Collection<String> batchId);
 }
