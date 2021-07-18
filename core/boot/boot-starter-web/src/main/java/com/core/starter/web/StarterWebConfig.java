b354f4d3d773615f4ae81d76338efcdd93babe70
luyi <893431435@qq.com>
2021-07-18 14:38:34 +0800

    fix:清理无用的代码依赖

--- core/boot/boot-starter-web/src/main/java/com/core/starter/web/StarterWebConfig.java
+++ core/boot/boot-starter-web/src/main/java/com/core/starter/web/StarterWebConfig.java
@@ -4,6 +4,8 @@ package com.core.starter.web;
 
 import com.core.starter.web.exception.GlobalExceptionHandler;
 import com.core.starter.web.response.BaseResponseBodyAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
