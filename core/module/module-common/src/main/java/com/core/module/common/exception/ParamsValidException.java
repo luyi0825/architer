1e8fff467cdb42990d569cabd4b08d7400ae51bb
luyi <893431435@qq.com>
2021-07-18 14:22:01 +0800

    fix:异常增加编码

--- core/module/module-common/src/main/java/com/core/module/common/exception/ParamsValidException.java
+++ core/module/module-common/src/main/java/com/core/module/common/exception/ParamsValidException.java
@@ -7,23 +7,43 @@ package com.core.module.common.exception;
  */
 public class ParamsValidException extends RuntimeException {
 
+    /**
+     * 异常编码，帮助异常快速定位
+     */
+    private int code = 2;
+
     public ParamsValidException() {
-        super();
+
+    }
+
+    public ParamsValidException(int code) {
+        this.code = code;
     }
 
     public ParamsValidException(String message) {
         super(message);
     }
 
-    public ParamsValidException(String message, Throwable cause) {
+    public ParamsValidException(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    public ParamsValidException(String message, Throwable cause, int code) {
         super(message, cause);
+        this.code = code;
     }
 
-    public ParamsValidException(Throwable cause) {
+    public ParamsValidException(Throwable cause, int code) {
         super(cause);
+        this.code = code;
     }
 
-    protected ParamsValidException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
     }
 }
--- core/module/module-common/src/main/java/com/core/module/common/exception/ServiceException.java
+++ core/module/module-common/src/main/java/com/core/module/common/exception/ServiceException.java
@@ -6,22 +6,43 @@ package com.core.module.common.exception;
  * @author luyi
  */
 public class ServiceException extends RuntimeException {
+    /**
+     * 异常编码，帮助异常快速定位
+     */
+    private int code = 1;
+
     public ServiceException() {
+
     }
 
     public ServiceException(String message) {
+
+    }
+
+    public ServiceException(int code) {
+        this.code = code;
+    }
+
+    public ServiceException(String message, int code) {
         super(message);
+        this.code = code;
     }
 
-    public ServiceException(String message, Throwable cause) {
+    public ServiceException(String message, Throwable cause, int code) {
         super(message, cause);
+        this.code = code;
     }
 
-    public ServiceException(Throwable cause) {
+    public ServiceException(Throwable cause, int code) {
         super(cause);
+        this.code = code;
     }
 
-    public ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
     }
 }
