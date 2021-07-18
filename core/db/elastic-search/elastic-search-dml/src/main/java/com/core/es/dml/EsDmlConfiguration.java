2584b584cc267c0a13a241220eb7de986c1dad49
luyi <893431435@qq.com>
2021-07-18 14:50:31 +0800

    feat:增加es-dml配置类

---
+++ core/db/elastic-search/elastic-search-dml/src/main/java/com/core/es/dml/EsDmlConfiguration.java
@@ -0,0 +1,9 @@
+package com.core.es.dml;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.core.es.dml")
+public class EsDmlConfiguration {
+}
