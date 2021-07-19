aabff591fd8815ac27e0042f40fa258de6add8fe
luyi <893431435@qq.com>
2021-07-19 09:39:17 +0800

    fix:增加Override

--- core/db/elastic-search/elastic-search-sync-consume/src/main/java/com/core/es/consumer/service/impl/SyncResultServiceImpl.java
+++ core/db/elastic-search/elastic-search-sync-consume/src/main/java/com/core/es/consumer/service/impl/SyncResultServiceImpl.java
@@ -18,10 +18,12 @@ import java.util.List;
 public class SyncResultServiceImpl implements SyncResultService {
     private SyncResultDao syncResultDao;
 
+    @Override
     public void insert(SyncResult syncResult) {
         syncResultDao.insert(syncResult);
     }
 
+    @Override
     public SyncResult findByBusinessKeyAndBatchId(String businessKey, String batchId) {
         return syncResultDao.findByBusinessKeyAndBatchId(businessKey, batchId);
     }
