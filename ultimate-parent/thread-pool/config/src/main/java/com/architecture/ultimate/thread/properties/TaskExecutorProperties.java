01e1e6aa385e1794275cc3ae32896cc585f1c732
luyi <893431435@qq.com>
2021-07-22 00:56:38 +0800

    fix:注释完善

--- ultimate-parent/thread-pool/config/src/main/java/com/architecture/ultimate/thread/properties/TaskExecutorProperties.java
+++ ultimate-parent/thread-pool/config/src/main/java/com/architecture/ultimate/thread/properties/TaskExecutorProperties.java
@@ -1,7 +1,6 @@
 package com.architecture.ultimate.thread.properties;
 
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
@@ -13,6 +12,9 @@ import java.util.Map;
  */
 @ConfigurationProperties(prefix = "customize.thread-pool", ignoreInvalidFields = true)
 public class TaskExecutorProperties {
+    /**
+     * key为线程池的标识，value为线程池配置信息
+     */
     private Map<String, ThreadPoolConfig> configs;
 
     public Map<String, ThreadPoolConfig> getConfigs() {
