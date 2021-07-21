d532714d67091c4430c01adba81f6da1111f88a5
luyi <893431435@qq.com>
2021-07-22 00:55:49 +0800

    feat:增加发送数据的校验器

---
+++ ultimate-parent/mq/mq-rabbit/src/main/java/com/architecture/ultimate/mq/rabbit/send/RabbitMqSendValid.java
@@ -0,0 +1,12 @@
+package com.architecture.ultimate.mq.rabbit.send;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author luyi
+ * rabbitMq发送校验器
+ * 针对一些消息头，以及数据标识进行判断
+ */
+@Component
+public class RabbitMqSendValid {
+}
