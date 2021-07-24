b6311b025857cdd84f0b918c5e70d59ca6b342aa
luyi <893431435@qq.com>
2021-07-24 17:24:37 +0800

    fix:增加回滚测试类

--- bussiness/server/search-server/src/main/java/com/business/search/SearchServer.java
+++ bussiness/server/search-server/src/main/java/com/business/search/SearchServer.java
@@ -1,8 +1,15 @@
 package com.business.search;
 
 
+import com.architecture.ultimate.utils.JsonUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * 搜索服务
@@ -10,9 +17,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author luyi
  */
 @SpringBootApplication
+@RestController
 public class SearchServer {
     public static void main(String[] args) {
         new SpringApplication(SearchServer.class).run(args);
     }
 
+    @PostMapping("/callback")
+    public void callBack(@RequestBody Map<String, Object> params) {
+        System.out.println(JsonUtils.toJsonString(params));
+    }
 }
