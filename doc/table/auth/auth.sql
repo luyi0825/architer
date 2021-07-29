ad205c93fc48ca22166505a1f1da336909f97649
luyi <893431435@qq.com>
2021-07-29 19:11:05 +0800

    sql:auth的sql修改

--- doc/table/auth/auth.sql
+++ doc/table/auth/auth.sql
@@ -1,11 +1,15 @@
-CREATE TABLE `t_auth_user_1`
+#创建数据库
+create database t_auth_master;
+
+CREATE TABLE `t_auth_user`
 (
-    `id`            bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `id`            bigint(20) unsigned COMMENT '主键',
     `username`      varchar(100) NOT NULL COMMENT '用户名',
     `password`      varchar(50)  NOT NULL COMMENT '密码',
     `mail`          varchar(50)  NOT NULL COMMENT '邮件',
-    `lastLoginIp`   varchar(100) DEFAULT NULL COMMENT '最后登录IP',
-    `errorCount`    smallint(6) NOT NULL DEFAULT 0 COMMENT '错误次数',
-    `lastLoginTime` date         DEFAULT NULL COMMENT '最后登录时间',
+    `lastLoginIp`   varchar(100)          DEFAULT NULL COMMENT '最后登录IP',
+    `errorCount`    smallint(6)  NOT NULL DEFAULT 0 COMMENT '错误次数',
+    `lastLoginTime` date                  DEFAULT NULL COMMENT '最后登录时间',
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3
