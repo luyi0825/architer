5f62a46fdc60145a4340c6902c8f06aae4106b70
luyi <893431435@qq.com>
2021-07-30 17:03:08 +0800

    fix:SubModule修改

--- bussiness/modules/message/captcha-service-impl/src/main/java/com/business/message/captcha/MessageCaptchaModule.java
+++ bussiness/modules/message/captcha-service-impl/src/main/java/com/business/message/captcha/MessageCaptchaModule.java
@@ -1,12 +1,12 @@
 package com.business.message.captcha;
 
-import com.architecture.ultimate.module.common.Module;
+import com.architecture.ultimate.module.common.SubModule;
 
 /**
  * 增加消息服务，验证码模块
  *
  * @author luyi
  */
-@Module(name = "message-captcha", caption = "消息服务-验证码模块")
+@SubModule(name = "message-captcha", caption = "消息服务-验证码模块")
 public class MessageCaptchaModule {
 }
