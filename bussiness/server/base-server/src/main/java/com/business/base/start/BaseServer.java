0a87df0f1fca43afccc6abcb3bb765137dffd947
luyi <893431435@qq.com>
2021-08-01 15:47:55 +0800

    feat:开启缓存

--- bussiness/server/base-server/src/main/java/com/business/base/start/BaseServer.java
+++ bussiness/server/base-server/src/main/java/com/business/base/start/BaseServer.java
@@ -1,15 +1,16 @@
 package com.business.base.start;
 
+import com.architecture.ultimate.cache.common.annotation.EnableCustomCaching;
 import com.architecture.ultimate.starter.web.module.ModulesBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * 基础数据服务
  *
  * @author luyi
  */
 @SpringBootApplication
+@EnableCustomCaching
 public class BaseServer {
     public static void main(String[] args) {
         Class<?>[] modules = new ModulesBuilder().buildMoules(BaseServer.class);
