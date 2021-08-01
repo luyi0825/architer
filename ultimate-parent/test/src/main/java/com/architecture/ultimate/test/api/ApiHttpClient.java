725db52df5d611f8d6227a1acff8de54cefec29d
luyi <893431435@qq.com>
2021-08-01 23:04:32 +0800

    feat:增加ApiHttpClient

---
+++ ultimate-parent/test/src/main/java/com/architecture/ultimate/test/api/ApiHttpClient.java
@@ -0,0 +1,19 @@
+package com.architecture.ultimate.test.api;
+
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+/**
+ * @author luyi
+ * 构建api测试的httpClient
+ */
+public class ApiHttpClient {
+    public static HttpClient build() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(6))
+                .build();
+    }
+}
