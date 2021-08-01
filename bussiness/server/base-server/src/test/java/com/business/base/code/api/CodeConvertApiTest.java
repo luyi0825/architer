3ae049b48d0f03746602ff8a4dab10053794acbd
luyi <893431435@qq.com>
2021-08-02 00:08:48 +0800

    test:增加测试类

--- bussiness/server/base-server/src/test/java/com/business/base/code/api/CodeConvertApiTest.java
+++ bussiness/server/base-server/src/test/java/com/business/base/code/api/CodeConvertApiTest.java
@@ -1,9 +1,9 @@
-package com.business.base.area.api.impl;
-
+package com.business.base.code.api;
 
 import com.architecture.ultimate.module.common.StatusCode;
 import com.architecture.ultimate.module.common.response.BaseResponse;
 import com.architecture.ultimate.test.api.ApiHttpClient;
+import com.business.base.code.entity.CodeConvert;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,30 +14,35 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 
-@RunWith(value = SpringRunner.class)
+/**
+ * @see CodeConvertApi
+ */
 @SpringBootTest
-public class StandardAreaApiImplTest {
+@RunWith(value = SpringRunner.class)
+public class CodeConvertApiTest {
 
     @Value("${api-host}")
     private String apiHost;
 
+
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-
     @Test
-    public void findByParentId() throws IOException, InterruptedException {
+    public void add() throws IOException, InterruptedException {
+        CodeConvert codeConvert = new CodeConvert();
+        codeConvert.setCode("test-002");
+        codeConvert.setCaption("测试");
+        codeConvert.setRemark("测试");
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiHost + "/standardAreaApi/findByParentId/0"))
+                .uri(URI.create(apiHost + "/codeConvertApi/add"))
                 .header("Content-Type", "application/json")
-                .GET()
+                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(codeConvert)))
                 .build();
         HttpResponse<String> response = ApiHttpClient.build().send(request, HttpResponse.BodyHandlers.ofString());
         BaseResponse baseResponse = objectMapper.readValue(response.body(), BaseResponse.class);
-        Assert.assertEquals(StatusCode.SUCCESS.getCode(), (int) baseResponse.getCode());
+        Assert.assertTrue(baseResponse.getCode() == 200 || baseResponse.getCode() == 100);
     }
 }
