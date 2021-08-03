bfccb14d97191fa37c35e02fd1f8c3d83b58ff3f
luyi <893431435@qq.com>
2021-08-04 01:48:02 +0800

    fix: 版本升级

--- bussiness/server/base-server/src/main/java/com/business/base/start/BaseServer.java
+++ bussiness/server/base-server/src/main/java/com/business/base/start/BaseServer.java
@@ -4,6 +4,8 @@ import com.architecture.ultimate.cache.common.annotation.EnableCustomCaching;
 import com.architecture.ultimate.starter.web.module.ModulesBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * 基础数据服务
  *
@@ -11,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableCustomCaching
+@EnableDiscoveryClient
 public class BaseServer {
     public static void main(String[] args) {
         Class<?>[] modules = new ModulesBuilder().buildMoules(BaseServer.class);
