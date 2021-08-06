4941530e2a2531f220d95c34bc8d42f83c1d7872
luyi <893431435@qq.com>
2021-08-07 01:06:43 +0800

    fix:gateway修改

--- bussiness/server/gateway-server/src/main/java/com/bussiness/GlobalExceptionHandler.java
+++ bussiness/server/gateway-server/src/main/java/com/bussiness/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ package com.bussiness;
 
 import com.architecture.ultimate.module.common.StatusCode;
 import com.architecture.ultimate.module.common.exception.ServiceException;
-import com.architecture.ultimate.module.common.response.BaseResponse;
+import com.architecture.ultimate.module.common.response.ResponseResult;
 
 import com.architecture.ultimate.module.common.response.R;
 import lombok.extern.slf4j.Slf4j;
@@ -48,7 +48,7 @@ public class GlobalExceptionHandler {
      * @return 统一的返回结果
      */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public BaseResponse webMvcValidException(MethodArgumentNotValidException exception) {
+    public ResponseResult webMvcValidException(MethodArgumentNotValidException exception) {
         //得到所有的异常信息
         return getBindExceptionBaseResponse(exception.getBindingResult());
     }
@@ -60,7 +60,7 @@ public class GlobalExceptionHandler {
      * @return 统一的返回结果
      */
     @ExceptionHandler(WebExchangeBindException.class)
-    public BaseResponse gateWayValidException(WebExchangeBindException exception) {
+    public ResponseResult gateWayValidException(WebExchangeBindException exception) {
         return getBindExceptionBaseResponse(exception.getBindingResult());
     }
 
@@ -72,18 +72,18 @@ public class GlobalExceptionHandler {
      * 处理系统异常
      */
     @ExceptionHandler(value = Throwable.class)
-    public BaseResponse exceptionHandler(Exception e) {
-        BaseResponse baseResponse;
+    public ResponseResult exceptionHandler(Exception e) {
+        ResponseResult baseResponse;
         if (e instanceof ServiceException) {
             //业务校验抛出的异常
-            baseResponse = new BaseResponse(StatusCode.SERVICE_EXCEPTION.getCode(), e.getMessage(), null);
+            baseResponse = new ResponseResult(StatusCode.SERVICE_EXCEPTION.getCode(), e.getMessage(), null);
             // -----------请求参数校验异常------
         } else if (e instanceof BindException) {
             baseResponse = getBindExceptionBaseResponse(((BindException) e).getBindingResult());
         } else {
             log.error(e.getMessage(), e);
             //其他的一些异常，是程序不可控制的
-            baseResponse = new BaseResponse(StatusCode.SYSTEM_EXCEPTION.getCode(), StatusCode.SYSTEM_EXCEPTION.getMessage(), null);
+            baseResponse = new ResponseResult(StatusCode.SYSTEM_EXCEPTION.getCode(), StatusCode.SYSTEM_EXCEPTION.getMessage(), null);
             return baseResponse;
         }
         log.error(e.getMessage(), e);
@@ -96,8 +96,8 @@ public class GlobalExceptionHandler {
      * 处理系统异常
      */
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public BaseResponse exceptionHandler(IllegalArgumentException e) {
-        return new BaseResponse(400, e.getMessage(), null);
+    public ResponseResult exceptionHandler(IllegalArgumentException e) {
+        return new ResponseResult(400, e.getMessage(), null);
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
@@ -118,16 +118,16 @@ public class GlobalExceptionHandler {
      * @author luyi
      * @date 2020/12/20
      */
-    private BaseResponse getBindExceptionBaseResponse(BindingResult bindingResult) {
+    private ResponseResult getBindExceptionBaseResponse(BindingResult bindingResult) {
         List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
-        BaseResponse baseResponse=  new BaseResponse(StatusCode.PARAMS_VALID_EXCEPTION.getCode(),  fieldErrorList.get(0).getDefaultMessage());
-        if(log.isDebugEnabled()){
+        ResponseResult baseResponse = new ResponseResult(StatusCode.PARAMS_VALID_EXCEPTION.getCode(), fieldErrorList.get(0).getDefaultMessage());
+        if (log.isDebugEnabled()) {
             Map<String, String> errorInfoMap = new HashMap<>(fieldErrorList.size());
             //TODO 调试环境开启
             fieldErrorList.forEach(error -> {
                 errorInfoMap.put(error.getField(), error.getDefaultMessage());
             });
-           // baseResponse.setTip(JSON.toJSONString(errorInfoMap));
+            // baseResponse.setTip(JSON.toJSONString(errorInfoMap));
         }
         return baseResponse;
     }
