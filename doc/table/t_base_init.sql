61b9e80e48dba4cc8fd3ec83f95017ee45a5165d
luyi <893431435@qq.com>
2021-08-07 15:43:07 +0800

    sql:t_base_code_map_item修改

--- doc/table/t_base_init.sql
+++ doc/table/t_base_init.sql
@@ -25,17 +25,17 @@ CREATE TABLE `t_base_code_map_item`
 (
     `id`           bigint unsigned primary key AUTO_INCREMENT COMMENT '主键ID',
     `item_code`    varchar(30) NOT NULL COMMENT '编码',
-    `convert_code` varchar(30) NOT NULL COMMENT 't_base_code_convert编码',
-    `caption`      varchar(50) NOT NULL COMMENT '对应中文',
+    `code`         varchar(30) NOT NULL COMMENT 't_base_code_map编码',
+    `item_caption` varchar(50) NOT NULL COMMENT '对应中文',
     `remark`       varchar(100) DEFAULT NULL COMMENT '备注',
     `update_time`  date         DEFAULT NULL COMMENT '更新时间',
     `create_time`  date         DEFAULT NULL COMMENT '创建时间',
     `update_user`  varchar(100) DEFAULT NULL COMMENT '更新人',
     `create_user`  varchar(100) DEFAULT NULL COMMENT '创建人',
     #增加外键
-    FOREIGN KEY (convert_code) REFERENCES t_base_code_map (code),
-    ##唯一键
-    unique key (convert_code, item_code)
+    FOREIGN KEY (code) REFERENCES t_base_code_map (code),
+    ## 唯一索引
+    unique index t_base_code_map_code_item_code (code, item_code)
 ) ENGINE = InnoDB COMMENT ='代码集项'
   DEFAULT CHARSET = utf8mb3;
 #-----------------------------t_base_standard_area#标准区划
