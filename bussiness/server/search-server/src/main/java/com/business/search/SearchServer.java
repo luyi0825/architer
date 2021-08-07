08730639a6707d07555e10338160d244bdddc229
luyi <893431435@qq.com>
2021-08-07 21:37:16 +0800

    fix:删除多余的代码

--- bussiness/server/search-server/src/main/java/com/business/search/SearchServer.java
+++ bussiness/server/search-server/src/main/java/com/business/search/SearchServer.java
@@ -1,15 +1,10 @@
 package com.business.search;
 
 
-import com.architecture.ultimate.utils.JsonUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 /**
  * 搜索服务
@@ -23,8 +18,4 @@ public class SearchServer {
         new SpringApplication(SearchServer.class).run(args);
     }
 
-    @PostMapping("/callback")
-    public void callBack(@RequestBody Map<String, Object> params) {
-        System.out.println(JsonUtils.toJsonString(params));
-    }
 }
