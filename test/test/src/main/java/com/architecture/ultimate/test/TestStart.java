b1eb01dfabad06f1125b107a8af2f0a9b5c5095b
luyi <893431435@qq.com>
2021-08-08 22:56:43 +0800

    fix:删除多余的import

--- test/test/src/main/java/com/architecture/ultimate/test/TestStart.java
+++ test/test/src/main/java/com/architecture/ultimate/test/TestStart.java
@@ -1,13 +1,10 @@
 package com.architecture.ultimate.test;
 
 
-;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
