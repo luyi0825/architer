ee9c2f38956777b92adcfc62cd49266c7c5632ed
luyi <893431435@qq.com>
2021-08-10 19:01:34 +0800

    fix:全局异常处理修改

--- ultimate-parent/web-starter/boot-starter-web-mvc/src/main/java/com/architecture/ultimate/starter/web/exception/GlobalExceptionHandler.java
+++ ultimate-parent/web-starter/boot-starter-web-mvc/src/main/java/com/architecture/ultimate/starter/web/exception/GlobalExceptionHandler.java
@@ -6,17 +6,25 @@ import com.architecture.ultimate.module.common.exception.ParamsValidException;
 import com.architecture.ultimate.module.common.exception.ServiceException;
 import com.architecture.ultimate.module.common.response.ResponseResult;
 import com.architecture.ultimate.module.common.response.R;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 全局异常处理
@@ -26,89 +34,125 @@ import java.util.List;
  * @author luyi
  * @date 2020/12/9
  */
-@Log4j2
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     public GlobalExceptionHandler() {
-        log.info("初始化全局异常处理....");
-    }
-
-
-    @Value("${spring.application.name:12}")
-    private String applicationName;
-
-    /**
-     * webMvc参数校验异常: JSR303校验 - @Valid
-     *
-     * @param exception 异常信息
-     * @return 统一的返回结果
-     */
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseResult webMvcValidException(MethodArgumentNotValidException exception) {
-        //得到所有的异常信息
-        return getBindExceptionBaseResponse(exception.getBindingResult());
+        logger.info("初始化全局异常处理");
     }
 
 
     /**
      * @author luyi
      * @date 2020/8/22
-     * //@TODO 记录系统不可预期的异常,
      * 处理系统异常
      */
     @ExceptionHandler(value = Throwable.class)
     public ResponseResult exceptionHandler(Exception e) {
-        ResponseResult baseResponse;
+        //处理校验异常
+        ResponseResult responseResult = this.handleValidException(e);
+        if (responseResult != null) {
+            return responseResult;
+        }
+
         if (e instanceof ServiceException) {
             //业务校验抛出的异常
-            baseResponse = new ResponseResult(ResponseStatusEnum.SERVICE_EXCEPTION.getCode(), e.getMessage(), null);
+            responseResult = new ResponseResult(ResponseStatusEnum.SERVICE_EXCEPTION.getCode(), e.getMessage(), null);
             // -----------请求参数校验异常------
-        } else if (e instanceof BindException) {
-            baseResponse = getBindExceptionBaseResponse(((BindException) e).getBindingResult());
-        } else if (e instanceof ParamsValidException) {
-            return new ResponseResult(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode(), e.getMessage());
         } else {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             //其他的一些异常，是程序不可控制的
-            baseResponse = new ResponseResult(ResponseStatusEnum.SYSTEM_EXCEPTION.getCode(), ResponseStatusEnum.SYSTEM_EXCEPTION.getMessage(), null);
-            return baseResponse;
+            responseResult = new ResponseResult(ResponseStatusEnum.SYSTEM_EXCEPTION.getCode(), ResponseStatusEnum.SYSTEM_EXCEPTION.getMessage(), null);
+            return responseResult;
         }
-        log.error(e.getMessage(), e);
-        return baseResponse;
+        logger.error(e.getMessage(), e);
+        return responseResult;
     }
 
     /**
-     * @author luyi
-     * @date 2020/8/22
-     * 处理系统异常
+     * 处理校验的异常
      */
-    @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseResult exceptionHandler(IllegalArgumentException e) {
-        return new ResponseResult(400, e.getMessage(), null);
+    private ResponseResult handleValidException(Exception e) {
+        //缺少参数抛出的异常
+        if (e instanceof MissingServletRequestParameterException) {
+            return this.getMissingServletRequestParameterExceptionResponseResult((MissingServletRequestParameterException) e);
+        }
+        //单个校验失败的异常
+        if (e instanceof ConstraintViolationException) {
+            return constraintViolationExceptionResponseResult(e);
+        }
+        //post请求的对象参数校验失败后抛出的异常
+        if (e instanceof MethodArgumentNotValidException) {
+            return getMethodArgumentNotValidExceptionResponseResult(e);
+        }
+        //get请求的对象参数校验失败后抛出的异常
+        if (e instanceof BindException) {
+            return getBindExceptionResponseResult((BindException) e);
+        }
+        //自定义的参数校验异常
+        if (e instanceof ParamsValidException) {
+            return new ResponseResult(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode(), e.getMessage());
+        }
+        return null;
     }
+
+    /**
+     * post请求的对象参数校验失败后抛出的异常
+     */
+    private ResponseResult getMethodArgumentNotValidExceptionResponseResult(Exception e) {
+        List<ObjectError> errors = ((MethodArgumentNotValidException) e).getBindingResult().getAllErrors();
+        Map<String, String> errorsMap = new HashMap<>(errors.size());
+        errors.forEach(error -> {
+            errorsMap.put(error.getObjectName(), error.getDefaultMessage());
+
+        });
+        return new ResponseResult(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode(), null, errorsMap);
+    }
+
+    /**
+     * 单个校验失败的异常
+     */
+    private ResponseResult constraintViolationExceptionResponseResult(Exception e) {
+        Set<ConstraintViolation<?>> sets = ((ConstraintViolationException) e).getConstraintViolations();
+        if (!CollectionUtils.isEmpty(sets)) {
+            Map<String, String> errors = new HashMap<>();
+            sets.forEach(error -> {
+                if (error instanceof FieldError) {
+                    FieldError fieldError = (FieldError) error;
+                    errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+                }
+            });
+            ResponseResult responseResult = new ResponseResult();
+            responseResult.setCode(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode());
+            responseResult.setData(errors);
+            return responseResult;
+        }
+        return null;
+    }
+
+    /**
+     * get请求的对象参数校验失败后抛出的异常是BindException
+     */
+    private ResponseResult getBindExceptionResponseResult(BindException bindException) {
+        List<FieldError> fieldErrorList = bindException.getFieldErrors();
+        return new ResponseResult(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode(), fieldErrorList.get(0).getDefaultMessage());
+    }
+
+    /**
+     * 缺少参数的异常
+     */
+    private ResponseResult getMissingServletRequestParameterExceptionResponseResult(MissingServletRequestParameterException e) {
+        String message = MessageFormat.format("缺少参数{0}", e.getParameterName());
+        return new ResponseResult(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode(), message);
+    }
+
 
     @ExceptionHandler(DuplicateKeyException.class)
     public R handleDuplicateKeyException(DuplicateKeyException e) {
-        log.error(e.getMessage(), e);
+        logger.error(e.getMessage(), e);
         return R.error("数据库中已存在该记录");
     }
 
-//    @ExceptionHandler(AuthorizationException.class)
-//    public R handleAuthorizationException(AuthorizationException e){
-//        logger.error(e.getMessage(), e);
-//        return R.error("没有权限，请联系管理员授权");
-//    }
-
-    /**
-     * 描述:得到参数校验BindException的返回结果
-     *
-     * @author luyi
-     * @date 2020/12/20
-     */
-    private ResponseResult getBindExceptionBaseResponse(BindingResult bindingResult) {
-        List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
-        return new ResponseResult(ResponseStatusEnum.PARAMS_VALID_EXCEPTION.getCode(), fieldErrorList.get(0).getDefaultMessage());
-    }
 
 }
