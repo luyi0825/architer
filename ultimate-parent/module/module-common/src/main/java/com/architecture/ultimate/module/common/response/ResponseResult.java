baed76c51e6aacd588888351292fa67ff8be6cc2
luyi <893431435@qq.com>
2021-08-10 22:58:30 +0800

    fix:响应结果重写hash和equals,删除多余的方法

--- ultimate-parent/module/module-common/src/main/java/com/architecture/ultimate/module/common/response/ResponseResult.java
+++ ultimate-parent/module/module-common/src/main/java/com/architecture/ultimate/module/common/response/ResponseResult.java
@@ -4,6 +4,7 @@ package com.architecture.ultimate.module.common.response;
 import com.architecture.ultimate.module.common.ResponseStatusEnum;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * @author ly
@@ -78,9 +79,7 @@ public class ResponseResult implements Serializable {
      * 成功
      */
     public static ResponseResult ok(Object data) {
-        ResponseResult baseResponse = new ResponseResult();
-        baseResponse.setCode(ResponseStatusEnum.SUCCESS.getCode());
-        baseResponse.setMessage(ResponseStatusEnum.SUCCESS.getMessage());
+        ResponseResult baseResponse = ResponseResult.ok();
         baseResponse.setData(data);
         return baseResponse;
     }
@@ -95,16 +94,21 @@ public class ResponseResult implements Serializable {
         return baseResponse;
     }
 
-    /**
-     * 失败
-     */
-    public static ResponseResult fail(Object data) {
-        ResponseResult baseResponse = new ResponseResult();
-        baseResponse.setCode(ResponseStatusEnum.SERVICE_EXCEPTION.getCode());
-        baseResponse.setData(data);
-        baseResponse.setMessage(ResponseStatusEnum.SERVICE_EXCEPTION.getMessage());
-        return baseResponse;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ResponseResult that = (ResponseResult) o;
+        return Objects.equals(code, that.code) && Objects.equals(message, that.message) && Objects.equals(data, that.data);
     }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, message, data);
+    }
 }
