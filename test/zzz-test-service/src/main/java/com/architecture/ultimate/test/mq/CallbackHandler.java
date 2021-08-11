f069d79035d3b71e6d9c2d20f5bea540b3ae40ec
luyi <893431435@qq.com>
2021-08-11 23:30:54 +0800

    test:rabbitMq消息确认

--- test/zzz-test-service/src/main/java/com/architecture/ultimate/test/mq/CallbackHandler.java
+++ test/zzz-test-service/src/main/java/com/architecture/ultimate/test/mq/CallbackHandler.java
@@ -4,6 +4,7 @@ package com.architecture.ultimate.test.mq;
 import com.architecture.ultimate.mq.rabbit.callback.ConfirmCallbackHandler;
 import com.architecture.ultimate.mq.rabbit.callback.ReturnCallbackHandler;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.ReturnedMessage;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.stereotype.Component;
 
@@ -25,7 +26,7 @@ public class CallbackHandler implements ConfirmCallbackHandler, ReturnCallbackHa
     }
 
     @Override
-    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-        System.out.println("in return");
+    public void returnedMessage(ReturnedMessage returned) {
+
     }
 }
