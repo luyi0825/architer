9f3b1769f7e2bb19fbe000fdd5ef36933c08d414
luyi <893431435@qq.com>
2021-08-13 10:46:17 +0800

    fix:注释log代码

--- ultimate-parent/log/log-executor/src/main/java/com/architecture/ultimate/log/executor/LogExecutor.java
+++ ultimate-parent/log/log-executor/src/main/java/com/architecture/ultimate/log/executor/LogExecutor.java
@@ -1,16 +1,16 @@
-package com.architecture.ultimate.log.executor;
-
-import com.architecture.ultimate.thread.CommonTaskExecutor;
-import com.architecture.ultimate.thread.properties.ThreadPoolConfig;
-
-/**
- * @author luyi
- */
-public class LogExecutor extends CommonTaskExecutor {
-    public LogExecutor(ThreadPoolConfig threadPoolConfig) {
-        super(threadPoolConfig);
-    }
-//    public LogExecutor(LogProperties taskExecutorProperties) {
-//        super(taskExecutorProperties);
+//package com.architecture.ultimate.log.executor;
+//
+//import com.architecture.ultimate.thread.CommonTaskExecutor;
+//import com.architecture.ultimate.thread.properties.ThreadPoolConfig;
+//
+///**
+// * @author luyi
+// */
+//public class LogExecutor extends CommonTaskExecutor {
+//    public LogExecutor(ThreadPoolConfig threadPoolConfig) {
+//        super(threadPoolConfig);
 //    }
-}
+////    public LogExecutor(LogProperties taskExecutorProperties) {
+////        super(taskExecutorProperties);
+////    }
+//}
