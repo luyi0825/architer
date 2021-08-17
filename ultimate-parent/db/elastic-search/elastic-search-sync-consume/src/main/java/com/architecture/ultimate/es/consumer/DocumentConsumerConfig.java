832c2ac51796516e6e2392c168ed7cc9df3df5da
luyi <893431435@qq.com>
2021-08-17 11:31:35 +0800

    fix:清理无用的import

--- ultimate-parent/db/elastic-search/elastic-search-sync-consume/src/main/java/com/architecture/ultimate/es/consumer/DocumentConsumerConfig.java
+++ ultimate-parent/db/elastic-search/elastic-search-sync-consume/src/main/java/com/architecture/ultimate/es/consumer/DocumentConsumerConfig.java
@@ -1,19 +1,12 @@
 package com.architecture.ultimate.es.consumer;
 
 
-import com.architecture.ultimate.thread.properties.TaskExecutorProperties;
-import com.architecture.ultimate.thread.properties.ThreadPoolConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.util.CollectionUtils;
 
-import java.util.Map;
 
 /**
  * @author luyi
