8b685399124e9a130ff967e73a3d3cea582c5eb1
luyi <893431435@qq.com>
2021-08-22 01:15:45 +0800

    fix:去掉多余的import

--- ultimate-parent/context/src/main/java/com/architecture/context/module/SubModule.java
+++ ultimate-parent/context/src/main/java/com/architecture/context/module/SubModule.java
@@ -1,12 +1,7 @@
 package com.architecture.context.module;
 
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 import java.lang.annotation.*;
 
