eb90d478d6574e9dc78219574ccf5478ecf7bfaa
luyi <893431435@qq.com>
2021-08-23 23:26:06 +0800

    fix:方法返回值功能函数说明

--- ultimate-parent/context/src/main/java/com/architecture/context/cache/proxy/MethodReturnValueFunction.java
+++ ultimate-parent/context/src/main/java/com/architecture/context/cache/proxy/MethodReturnValueFunction.java
@@ -1,14 +1,15 @@
 package com.architecture.context.cache.proxy;
 
 /**
- * 返回值功能函数：控制返回值和多次调用
+ * 方法返回值功能函数：控制返回值和多次调用
  *
  * @author luyi
  */
-public interface ReturnValueFunction {
+public interface MethodReturnValueFunction {
     /**
      * 处理
      *
+     * @return 调用方法后返回的值
      * @throws Throwable 调用方法抛出的异常
      */
     default Object proceed() throws Throwable {
