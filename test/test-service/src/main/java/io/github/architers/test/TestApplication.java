031d3e8f0d90c7d55df2207fc63ba5341b75da57
luyi <893431435@qq.com>
2022-09-27 17:46:25 +0800

    test代码提交

---
+++ test/test-service/src/main/java/io/github/architers/test/TestApplication.java
@@ -0,0 +1,15 @@
+package io.github.architers.test;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luyi
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
