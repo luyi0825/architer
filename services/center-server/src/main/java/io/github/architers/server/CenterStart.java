ad7711caf98e288b2503e90469476ad44c51684e
893431435@qq.com <19940825Ls>
2022-10-23 02:12:55 +0800

    fix:模块重构

--- services/center-server/src/main/java/io/github/architers/server/CenterStart.java
+++ services/center-server/src/main/java/io/github/architers/server/CenterStart.java
@@ -1,6 +1,5 @@
 package io.github.architers.server;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
